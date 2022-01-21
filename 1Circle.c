7942f01a2ffdf1f154ec047ec44854389dff0cba
LeesMond <ljm71555555@gmail.com>
2022-01-21 20:48:29 +0900

    no

--- 1Circle.c
+++
@@ -1,9 +0,0 @@
-//#include<stdio.h>
-//
-//int main() {
-//	int n1;
-//	printf("원의 넓이 계산기 \n구하고자 하는 원의 반지름을 입력하시오 : ");
-//	scanf("%d", &n1);
-//	printf("원의 넓이는 : %.4f", n1 * n1 * 3.141592);
-//	return 0;
-//}
